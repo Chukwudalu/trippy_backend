c716ca795c05cebe761bfbf375bd1c6c00ea577e
Jeremiah Okwuolisa <jerryolisa@gmail.com>
2022-09-30 19:47:47 -0700

    working on cors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,7 +16,7 @@ router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
 // Protect all routes after this middleware
-// router.use(authController.protect)
+router.use(authController.protect)
 
 router.patch('/updatePassword', authController.updatePassword);
 
