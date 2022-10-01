5ce26a042aaa455e7d08e0e0f67c8daff8429aab
Jeremiah Okwuolisa <jerryolisa@gmail.com>
2022-09-30 18:01:33 -0700

    still working on cors

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cors(corsConfig))
 app.options('*', cors(corsConfig));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://trippy1.netlify.app');
     next();
 });
 
